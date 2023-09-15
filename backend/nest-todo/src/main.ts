f82efd23abbf0555789e14cc5f36bacbe9cab2a5
CrankyGus <amyol@CrankyGus.local>
2023-09-15 14:23:53 +0700

    add - dockerfile & update - readme

--- backend/nest-todo/src/main.ts
+++ backend/nest-todo/src/main.ts
@@ -4,6 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('/api');
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
   await app.listen(3001);
 }
 bootstrap();
